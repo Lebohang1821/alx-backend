61ab6b0042b48ccbe0aba79d95d2b303fd0a4231
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-05 17:43:55 +0200

    Update 3-hypermedia_del_pagination.py

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Deletion-resilient hypermedia pagination
+It removes-resilient hypermedia pagination
 """
 
 import csv
@@ -9,7 +9,7 @@ from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """Server class for pagination of database of popular baby names
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -18,7 +18,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """It retrieves cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -29,7 +29,7 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
+        """Retrieves dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
@@ -41,7 +41,7 @@ class Server:
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
-        get_hyper_index function
+        It provides deletion-resilient hypermedia pagination details
         """
         assert type(index) is int and index > 0
         assert type(page_size) is int and page_size > 0
